74fc19a99310c2fa9ef9d94682cd7a196aec768a
Bob <bobmasika@gmail.com>
2025-11-09 11:28:26 +0300

    2. Arguments check

---
+++ 2-arguments.js
@@ -0,0 +1,13 @@
+//Get the command-line arguments (excluding "node" and script name)
+const args = process.argv.slice(2);
+
+//Check how may arguments were passed
+if (args.lengh === 0){
+    console.log("No argument");
+} 
+else if (args.length === 1){
+    console.log("Argument found");
+}
+else {
+    console.log("Arguments found")
+}
