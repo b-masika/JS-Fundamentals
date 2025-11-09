a91e6579a929302aad61681315f93084dff0d2c7
Bob <bobmasika@gmail.com>
2025-11-09 12:01:38 +0300

    7. Print C multiple times

---
+++ 7-multi_c.js
@@ -0,0 +1,16 @@
+//First argument
+const arg = process.argv[2];
+
+//Convert to integer
+const x = parseInt(arg);
+
+//Check if x is a valid positive integer
+if (isNaN(x) || x <= o) {
+    console.log("Missing number of occurrences");
+}
+else {
+    //Loop x times
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
+}
