4ec335b5e5b677ecbda1d21325cd300ca0e77e4e
Bob <bobmasika@gmail.com>
2025-11-09 11:13:28 +0300

    0. First constant, first print

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,5 +1,2 @@
-//Decare a constant variable names 'myVar' and assign if the string value "javascript is amazing"
 const myVar = "Javascript is amazing";
-
-//Print the value of 'myVar' to the console
 console.log(myVar);
